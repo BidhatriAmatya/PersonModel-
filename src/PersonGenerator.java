7b6f8df7da73b2bed21cff015f19e31ffe457924
Bidhatri Amatya <bidhatriamatya@Bidhatris-Air.lan>
2022-08-28 18:26:42 -0400

    Initial commit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.scanner;
+
+public class PersonGenerator {
+    Scanner in = new Scanner(System.in);
+    ArrayList<Person> people = new ArrayList<>();
+}
